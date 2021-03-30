f0747c4d1123c65666ed73cd022781164e25115a
vitaly <orangebester@gmail.com>
2021-03-30 18:53:20 +0300

    decompose functions

--- creator.py
+++ creator.py
@@ -10,24 +10,24 @@ API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
 # id of folder where you want to create other folders
 root_folder_id = '1kg2bc_DNPVwu8sbHp9AC3hHjuJh4OCse'
+# id of template spreadsheet that you need to copy
 template_spreadsheet_id = '1Y6Rp4_kDz02wnqbkNq3ZwS5j88HQeFu26l0wNB5wtcY'
-
-
-alphabet = list('abcdefghijklmnopqrstuvwxyz')
 
 
 def generate_id():
     """ Generates id each elements of which consist of random values """
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
     first = random.randint(0, 9)
     second = random.choice(alphabet)
     third = random.choice(alphabet)
     forth = random.choice(alphabet)
     fifth = random.randint(0, 9)
-    not_final_id = str(first) + second.upper() + \
+    alpha_id = str(first) + second.upper() + \
         third + forth.upper() + str(fifth)
-    return not_final_id
+    return alpha_id
 
 
 def generate_n_id(n):
@@ -44,53 +44,61 @@ def generate_n_id(n):
     return temp_storage
 
 
-def korpus_adder(lst):
-    """ Add 'корпус' and number of it if there are the same addresses in a row"""
-    lst1 = []
+def korpus_adder(adresses):
+    """ Add 'корпус' and it`s serial number if there are the same addresses in a row"""
+    temp = []
     n = 0
     m = 1
 
-    for a in lst:
-        if lst[n] == lst[n - 1]:
-            lst1.append(lst[n])
+    for adress in adresses:
+        if adresses[n] == adresses[n - 1]:
+            temp.append(adresses[n])
             if n > 0:
-                lst1[n] = lst1[n] + " / корпус " + str(m+1)
+                temp[n] = temp[n] + " / корпус " + str(m+1)
             if m == 1 and n != 0:
-                lst1[n - m] = lst1[n - m] + " / корпус 1"
+                temp[n - m] = temp[n - m] + " / корпус 1"
             m += 1
         else:
             m = 1
-            lst1.append(a)
+            temp.append(adress)
         n += 1
-    return lst1
+    return temp
 
 
-def general_func(n):
+def form_list_adresses(file1):
+    adresses = open(f"cities/{file1}", "r",
+                    encoding='utf-8').readlines()
+    final_adresses = [i.replace('\n', '') for i in adresses]
+    final_adresses = korpus_adder(final_adresses)
+    return final_adresses
+
+
+def create_json(output_result, file1):
+    with open(f"output/{file1}_sample.json", "w", encoding="utf-8") as output:
+        json.dump(output_result, output, ensure_ascii=False)
+    with open("meta.json", "r+", encoding="utf-8") as output:
+        data = json.load(output)
+        data.update(output_result)
+        output.seek(0)
+        json.dump(data, output, ensure_ascii=False)
+
+
+def create_names(final_adresses, final_storage):
+    sum_list = [a + "_" + b for a,
+                b in zip(final_adresses, final_storage)]
+    sum_list = tuple(sum_list)
+    return sum_list
+
+
+def general_func(count):
     """ Combining addresses and their unique id """
     file1 = sys.argv[1]
     if file1.endswith('.txt'):
-        # forming list of addreses and id`s
-        final_storage = generate_n_id(n)
-        adresses = open(f"cities/{file1}", "r",
-                        encoding='utf-8').readlines()
-        final_adresses = [i.replace('\n', '') for i in adresses]
-        final_adresses = korpus_adder(final_adresses)
+        final_storage = generate_n_id(count)
+        final_adresses = form_list_adresses(file1)
         output_result = dict(zip(final_adresses, final_storage))
-        # creating .json for storing addreses and id`s
-        with open(f"output/{file1}_sample.json", "w", encoding="utf-8") as output:
-            json.dump(output_result, output, ensure_ascii=False)
-        with open("meta.json", "r+", encoding="utf-8") as output:
-            data = json.load(output)
-            data.update(output_result)
-            output.seek(0)
-            json.dump(data, output, ensure_ascii=False)
-
-        # creating names for folders
-        # python creator.py dnepr_testcopy.txt
-
-        sum_list = [a + "_" + b for a,
-                    b in zip(final_adresses, final_storage)]
-        sum_list = tuple(sum_list)
+        create_json(output_result, file1)
+        sum_list = create_names(final_adresses, final_storage)
         return sum_list
     else:
         print(
