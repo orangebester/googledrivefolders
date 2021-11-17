fc5d0eb404e53c95550b3bad836462a5c473e389
Orangebe <orangebester@gmail.com>
2021-11-18 01:29:09 +0200

    deleted personal data

--- personal_data.py
+++
@@ -1,4 +0,0 @@
-# id of folder where you want to create other folders
-root_folder_id = '11d2ClxciCISVosjwcJ9-MHo1pXeysm4F'
-# id of template spreadsheet that you need to copy
-template_spreadsheet_id = '1Y6Rp4_kDz02wnqbkNq3ZwS5j88HQeFu26l0wNB5wtcY'
